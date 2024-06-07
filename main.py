a3a3e92de0022f9a778c72e9ed087ec958e0225a
Ricardo Volert <76571617+ricardovolert@users.noreply.github.com>
2024-06-07 15:18:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+## --------------------------------------------------- ##
+## Tina v.2.0.0 - Personal assistant                   ## 
+## This is ricardovolert's (i.e. me) version of        ##
+## fabrimatt's original project.                       ##
+## Miniaturized design of an Alexa clone. It will help ##
+## with temperature via Google, time and playing music ##
+## on YouTube and searches using Google and Wikipedia. ##
+## --------------------------------------------------- ##
+
+'''
+speech_recognition é usada para reconhecimento de voz.
+pyttsx4 é usada para conversão de texto em fala.
+datetime é usada para trabalhar com datas e horas.
+wikipedia é usada para buscar resumos na Wikipedia.
+pywhatkit é usada para tocar músicas no YouTube.
+requests é usada para fazer requisições HTTP.
+BeautifulSoup é usada para fazer parsing de HTML.
+'''
+
 import speech_recognition as sr
 import pyttsx4
 import datetime
@@ -7,6 +26,8 @@ import requests
 from bs4 import BeautifulSoup
 
 # Função para converter número para texto
+# numero_para_texto : converte um número em seu equivalente textual em português, 
+# retornando uma string correspondente ao número fornecido.
 def numero_para_texto(numero):
     numeros_texto = {
         0: 'zero', 1: 'um', 2: 'dois', 3: 'três', 4: 'quatro',
@@ -30,6 +51,9 @@ def numero_para_texto(numero):
     }
     return numeros_texto.get(numero, '')
 
+# Define uma função obter_previsao_tempo_google que faz uma requisição ao Google 
+# para obter a previsão do tempo de uma cidade específica, parseando o HTML da 
+# resposta e retornando o objeto BeautifulSoup se a requisição for bem-sucedida.
 def obter_previsao_tempo_google(cidade):
     url = f"https://www.google.com/search?q=previsão+do+tempo+{cidade.replace(' ', '+')}"
     headers = {
@@ -42,6 +66,8 @@ def obter_previsao_tempo_google(cidade):
     else:
         return None
 
+# Define uma função analisar_previsao que extrai e retorna informações 
+# detalhadas sobre a previsão do tempo a partir do HTML parseado.
 def analisar_previsao(soup):
     cidade = soup.find("div", attrs={"id": "wob_loc"}).text
     tempo = soup.find("span", attrs={"id": "wob_dc"}).text
@@ -61,10 +87,15 @@ def analisar_previsao(soup):
 
     return previsao_detalhada
 
+# Define uma função previsao_por_extenso que formata a previsão do tempo 
+# detalhada em uma string descritiva.
 def previsao_por_extenso(cidade, previsao_detalhada):
     descricao = f"Agora em {cidade} faz {previsao_detalhada['temperatura']}, {previsao_detalhada['tempo']} com umidade em {previsao_detalhada['umidade']} e ventos com velocidade de {previsao_detalhada['vento']}\n"
     return descricao
 
+# Define uma função google_search que faz uma busca no Google por uma 
+# query específica, remove elementos HTML desnecessários, extrai os 
+# resultados relevantes e os retorna como uma lista de strings.
 def google_search(query):
     url = f"https://www.google.com/search?q=texto+sobre+{query.replace(' ', '+')}"
     headers = {
@@ -89,17 +120,21 @@ def google_search(query):
     else:
         return []
 
+# Inicializa o reconhecedor de áudio (audio) e o conversor de texto para fala (maquina).
 audio = sr.Recognizer()
 maquina = pyttsx4.init()
 
-# Listar e configurar a voz desejada
+# Listar e configurar a voz desejada: Comentado, mas pode ser usado para listar e 
+# configurar a voz desejada para a conversão de texto para fala.
 # voices = maquina.getProperty('voices')
 # for voice in voices:
 #    print(f"ID: {voice.id}\nName: {voice.name}\nLanguage: {voice.languages}")
 # Escolher a voz (substitua pelo ID da voz desejada)
 # maquina.setProperty('voice', voices[0].id)  # Exemplo: setar para a primeira voz disponível
 
-
+# Define a função executa_comando que ouve um comando de voz, 
+# reconhece o texto usando a API do Google e, se o comando 
+# contiver "tina", remove essa palavra e repete o comando.
 def executa_comando():
     comando = ""  # Inicializa a variável comando
     try:
@@ -116,6 +151,14 @@ def executa_comando():
         print('Microfone não está ok')
     return comando
 
+# Define a função comando_voz_usuario que executa diferentes 
+# ações com base no comando de voz recebido, incluindo:
+# - Dizer a hora atual.
+# - Fazer uma pesquisa na Wikipedia.
+# - Fazer uma pesquisa no Google.
+# - Obter a previsão do tempo.
+# - Tocar uma música no YouTube.
+# - Encerrar o programa.
 def comando_voz_usuario():
     comando = executa_comando()
     if 'horas' in comando:
@@ -164,10 +207,13 @@ def comando_voz_usuario():
         maquina.runAndWait()
         exit()
 
+# Inicia o programa com uma mensagem de saudação usando conversão de texto para fala.
 maquina.say("Olá, meu nome é Tina. No que posso lhe ajudar hoje?")
 maquina.runAndWait()
 
+# Inicia um loop infinito que imprime as instruções de uso e 
+# chama a função comando_voz_usuario para continuar ouvindo e 
+# executando comandos de voz do usuário.
 while True:
     print("use: [horas] [pesquise ...] [temperatura] [tocar ...] [sair]")
     comando_voz_usuario()
-    
